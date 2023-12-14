2f71945605d325f742f5d2c73c7d78092d22fced
Johnny Houston <megajohnny@hotmail.co.uk>
2023-12-14 00:08:30 +0000

    Add helper to get puzzle input string

---
+++ src/util/getPuzzleInput.ts
@@ -0,0 +1,29 @@
+import { readFile, writeFile } from "fs/promises";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import wretch from "wretch";
+
+const cacheBasePath = join(fileURLToPath(import.meta.url), "../../../cache");
+
+export async function getPuzzleInput(day: number) {
+  const dayCachedPath = join(cacheBasePath, day + ".txt");
+
+  try {
+    return (await readFile(dayCachedPath)).toString();
+  } catch (e) {
+    if ((e as NodeJS.ErrnoException).code !== "ENOENT") {
+      throw e;
+    }
+
+    const fetchedInput = await wretch(
+      `https://adventofcode.com/2023/day/${day}/input`
+    )
+      .headers({ Cookie: `session=${process.env.ADVENT_OF_CODE_SESSION!}` })
+      .get()
+      .text();
+
+    writeFile(dayCachedPath, fetchedInput);
+
+    return fetchedInput;
+  }
+}
